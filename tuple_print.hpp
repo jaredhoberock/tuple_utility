db15c6176483df2ee538b1fdd4f7f0bec44cb3d9
Jared Hoberock <jaredhoberock@gmail.com>
2014-10-08 16:43:31 -0700

    Avoid defaulting to a std::string as tuple_print's delimiter

--- tuple_print.hpp
+++ tuple_print.hpp
@@ -2,35 +2,40 @@
 
 #include <iostream>
 #include <tuple>
-#include <string>
 #include "tuple_head.hpp"
 #include "tuple_tail.hpp"
 
-template<class Tuple>
+template<class Tuple, class T>
 typename std::enable_if<
   std::tuple_size<Tuple>::value == 0
 >::type
-tuple_print(std::ostream&, const Tuple& t, const std::string& = ", ")
+tuple_print(std::ostream&, const Tuple& t, const T&)
 {
 }
 
-template<class Tuple>
+template<class Tuple, class T>
 typename std::enable_if<
   std::tuple_size<Tuple>::value == 1
 >::type
-tuple_print(std::ostream& os, const Tuple& t, const std::string& = ", ")
+tuple_print(std::ostream& os, const Tuple& t, const T&)
 {
   os << std::get<0>(t);
 }
 
-template<class Tuple>
+template<class Tuple, class T>
 typename std::enable_if<
   (std::tuple_size<Tuple>::value > 1)
 >::type
-  tuple_print(std::ostream& os, const Tuple& t, const std::string& delimiter = ", ")
+  tuple_print(std::ostream& os, const Tuple& t, const T& delimiter)
 {
   os << tuple_head(t) << delimiter;
 
   tuple_print(os, tuple_tail(t), delimiter);
+}
+
+template<class Tuple>
+void tuple_print(std::ostream& os, const Tuple& t)
+{
+  tuple_print(os, t, ", ");
 }
 
