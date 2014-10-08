5b2ba86b3ad9073c68922a5e06f27422ec16313f
Jared Hoberock <jaredhoberock@gmail.com>
2014-10-08 12:42:29 -0700

    Rearrange arguments of tuple_map Make tuple_map variadic

--- demo.cpp
+++ demo.cpp
@@ -5,12 +5,23 @@ int main()
 {
   auto t = std::make_tuple(0, 1, 2, 3);
 
-  auto negative_t = tuple_map(t, [](int x)
-  {
-    return -x;
-  });
+  auto negative_t = tuple_map(std::negate<int>(), t);
 
   std::cout << "negative_t: " << std::get<0>(negative_t) << ", " << std::get<1>(negative_t) << ", " << std::get<2>(negative_t) << ", " << std::get<3>(negative_t) << std::endl;
+
+  auto zero_t = tuple_map(std::plus<int>(), t, negative_t);
+
+  std::cout << "zero_t: " << std::get<0>(zero_t) << ", " << std::get<1>(zero_t) << ", " << std::get<2>(zero_t) << ", " << std::get<3>(zero_t) << std::endl;
+
+  auto one_t = std::make_tuple(1,1,1);
+
+  auto two_t = std::make_tuple(2,2,2);
+
+  auto three_t = std::make_tuple(3,3,3);
+
+  auto six_t = tuple_map([](int x, int y, int z){return x + y + z;}, one_t, two_t, three_t);
+
+  std::cout << "six_t: " << std::get<0>(six_t) << ", " << std::get<1>(six_t) << ", " << std::get<2>(six_t) << std::endl;
 
   return 0;
 }
--- tuple_map.hpp
+++ tuple_map.hpp
@@ -5,60 +5,71 @@
 #include <type_traits>
 #include <utility>
 
-template<class Tuple, class Function1, class Function2, size_t... I>
-auto tuple_map_impl(Tuple&& t, Function1 f, Function2 make, std::index_sequence<I...>)
+template<size_t I, typename Function, typename... Tuples>
+auto __tuple_map_invoke(Function f, Tuples&&... ts)
+  -> decltype(
+       f(std::get<I>(std::forward<Tuples>(ts))...)
+     )
+{
+  return f(std::get<I>(std::forward<Tuples>(ts))...);
+}
+
+template<size_t... I, typename Function1, typename Function2, typename... Tuples>
+auto __tuple_map_with_make_impl(std::index_sequence<I...>, Function1 f, Function2 make, Tuples&&... ts)
   -> decltype(
        make(
-         f(std::get<I>(std::forward<Tuple>(t)))...
+         __tuple_map_invoke<I>(f, std::forward<Tuples>(ts)...)...
        )
      )
 {
   return make(
-    f(std::get<I>(std::forward<Tuple>(t)))...
+    __tuple_map_invoke<I>(f, std::forward<Tuples>(ts)...)...
   );
 }
 
-template<class Tuple, class Function1, class Function2>
-auto tuple_map(Tuple&& t, Function1 f, Function2 make)
+template<typename Function1, typename Function2, typename Tuple, typename... Tuples>
+auto tuple_map_with_make(Function1 f, Function2 make, Tuple&& t, Tuples&&... ts)
   -> decltype(
-       tuple_map_impl(
-         std::forward<Tuple>(t),
-         f,
-         make,
+       __tuple_map_with_make_impl(
          std::make_index_sequence<
            std::tuple_size<std::decay_t<Tuple>>::value
-         >()
+         >(),
+         f,
+         make,
+         std::forward<Tuple>(t),
+         std::forward<Tuples>(ts)...
        )
      )
 {
-  using indices = std::make_index_sequence<
-    std::tuple_size<std::decay_t<Tuple>>::value
-  >;
-  return tuple_map_impl(std::forward<Tuple>(t), f, make, indices());
+  return __tuple_map_with_make_impl(
+    std::make_index_sequence<
+      std::tuple_size<std::decay_t<Tuple>>::value
+    >(),
+    f,
+    make,
+    std::forward<Tuple>(t),
+    std::forward<Tuples>(ts)...
+  );
 }
 
-struct make_tuple_functor
+struct __tuple_maker
 {
-  template<class... Types>
-  auto operator()(Types&&... args)
+  template<class... T>
+  auto operator()(T&&... args)
     -> decltype(
-         std::make_tuple(std::forward<Types>(args)...)
+         std::make_tuple(std::forward<T>(args)...)
        )
   {
-    return std::make_tuple(std::forward<Types>(args)...);
+    return std::make_tuple(std::forward<T>(args)...);
   }
 };
 
-template<class Tuple, class Function>
-auto tuple_map(Tuple&& t, Function f)
+template<typename Function, typename Tuple, typename... Tuples>
+auto tuple_map(Function f, Tuple&& t, Tuples&&... ts)
   -> decltype(
-       tuple_map(
-         std::forward<Tuple>(t),
-         f,
-         make_tuple_functor()
-       )
+       tuple_map_with_make(f, __tuple_maker(), std::forward<Tuple>(t), std::forward<Tuples>(ts)...)
      )
 {
-  return tuple_map(std::forward<Tuple>(t), f, make_tuple_functor());
+  return tuple_map_with_make(f, __tuple_maker(), std::forward<Tuple>(t), std::forward<Tuples>(ts)...);
 }
 
